7a5562b0a6d6cd9410cc5cd4f2029e968d7076c7
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2023-02-07 19:24:42 +0100

    Initial files

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3
+EXPOSE 5000
+WORKDIR /src
+RUN pip install --no-cache-dir Flask
+COPY server.py /src/
+USER www-data
+CMD [ "python", "/srv/server.py" ]
---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from random import uniform
+app = Flask(__name__)
+
+minmax = range(1, 5)
+
+cars = [ { 'id': id, 'counter': 0. } for id in minmax]
+msg_error = {'error': 'ID is out of range! '}
+
+
+@app.route('/cars/<id>')
+def get_car(id):
+	if int(id) in minmax:
+		pt = int(id)-1
+		cars[pt]['counter'] += uniform(0., 100.)
+		return jsonify(cars[pt])
+	else:
+		msg = msg_error
+		msg['error'] += str(minmax)
+		return jsonify(msg_error)
+
+if __name__ == "__main__":
+    app.run(debug=False)
+
